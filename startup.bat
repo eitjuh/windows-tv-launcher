17d4eaf7dc4e8a9b51093836eafcf849ff414dd9
Jesse Eilers <eitjuh@gmail.com>
2014-07-28 10:48:34 +0200

    nas added

--- startup.bat
+++ startup.bat
@@ -1,2 +1,2 @@
-start cmd /k mongod --dbpath dbdata
-start cmd /k nodemon app/app.js
+start cmd /k mongod --dbpath C:/windows-tv-launcher/dbdata
+start cmd /k nodemon C:/windows-tv-launcher/app/app.js
