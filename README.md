49fdd1b5ac7e8c90f631d7e50c88f8f34f3e445a
Jesse Eilers <jesse.eilers@telfort.com>
2014-08-28 10:21:03 +0200

    added database and delete options and some additional fixes

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ For Windows
 1. Checkout repository
 2. Goto "app" folder and run command "npm install"
 3. In the root directory, create a folder called dbdata and create a folder called tmp
+4. "mongorestore --host localhost --port 27017 --db tv-launcher dump/tv-launcher" to import database (backup with "mongodump --host localhost --port 27017 --db tv-launcher")
 
 =Running=
 1. Start MongoDB from the root folder with the command: "mongod --dbpath dbdata" to start the DB
--- app/public/js/bootstrap.js
+++ app/public/js/bootstrap.js
@@ -1,37 +1,92 @@
 jQuery(document).ready(function($){
-	console.log('jquery init');
-	$('#scene').parallax();
+
+	if($(".sortable").length){
+		$(".sortable").sortable();
+	}
+
+	var Scene = (function() {
+
+		$('#scene').parallax();
 		
-	window.onresize = function() {
+		window.onresize = function() {
+			var $scene = $('#scene');
+				$scene[0].style.width = window.innerWidth + 'px';
+				$scene[0].style.height = window.innerHeight + 'px';
+		}
+		
 		var $scene = $('#scene');
 			$scene[0].style.width = window.innerWidth + 'px';
 			$scene[0].style.height = window.innerHeight + 'px';
-	}
-	
-	var $scene = $('#scene');
-		$scene[0].style.width = window.innerWidth + 'px';
-		$scene[0].style.height = window.innerHeight + 'px';
 		
-	$('.icon').on('click', function() {
-		var options = {};
-		if($(this).attr('data-launch-type') == "launchExe") {
-			options.type = $(this).attr('data-launch-type');
-			options.location = $(this).attr('data-launch-location');
-			options.activationTitle = $(this).attr('data-activation-title');
-		} else if($(this).attr('data-launch-type') == "launchCommand") {
-			options.type = $(this).attr('data-launch-type');
-			options.command = $(this).attr('data-command');
-			options.activationTitle = $(this).attr('data-activation-title');
-		} else if($(this).attr('data-launch-type') == "launchUrl") {
-			options.type = $(this).attr('data-launch-type');
-			options.href = $(this).attr('href');
-			options.activationTitle = "Chrome";
-		}
+	})();
+		
+	var Icon = (function() {
 	
-		$.ajax({
-			type: "POST",
-			url: "/launch-app",
-			data: options
+		
+		// on click
+		$('.icon').on('click', function() {
+			Icon.launch($(this));
 		});
-	});
+		
+		// on long click 
+		var longClick = (function() {
+			var timeoutId = 0;
+			var clickedIcon;
+			$('.icon').mousedown(function() {
+				clickedIcon = $(this);
+				timeoutId = setTimeout(function(){
+					Icon.addShake(clickedIcon);
+					return false;
+				}, 1000);
+			}).bind('mouseup', function() {
+				clearTimeout(timeoutId);
+				setTimeout(function(){
+					if(clickedIcon.hasClass('shake')){
+						clickedIcon.addClass('shake-can-be-removed');
+					}
+				},500);
+			});
+		})();
+		
+		//on shaky icon click
+		$(document).on('click', '.shake-can-be-removed', function() {
+			Icon.removeShake($(this));
+			return false;
+		});
+	
+		return {
+			launch: function(element) {
+				var options = {};
+				if($(element).attr('data-launch-type') == "launchExe") {
+					options.type = $(element).attr('data-launch-type');
+					options.location = $(element).attr('data-launch-location');
+					options.activationTitle = $(element).attr('data-activation-title');
+				} else if($(element).attr('data-launch-type') == "launchCommand") {
+					options.type = $(element).attr('data-launch-type');
+					options.command = $(element).attr('data-command');
+					options.activationTitle = $(element).attr('data-activation-title');
+				} else if($(element).attr('data-launch-type') == "launchUrl") {
+					options.type = $(element).attr('data-launch-type');
+					options.href = $(element).attr('href');
+					options.activationTitle = "Chrome";
+				}
+				if(!$(element).hasClass("disabled")) {
+					$.ajax({
+						type: "POST",
+						url: "/launch-app",
+						data: options
+					});
+				}
+			},
+			addShake: function(element) {
+				$(element).addClass('shake').addClass('disabled');
+				$(element).append('<img class="delete-app-icon" src="/img/delete-2-32.png" alt="Delete" />');
+			},
+			removeShake: function(element) {
+				$(element).removeClass('shake').removeClass('disabled').removeClass('shake-can-be-removed');
+				$('.delete-app-icon', $(element)).remove();
+			}
+		}
+	})();
+	
 });
